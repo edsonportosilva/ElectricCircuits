69338f3b2f0a2f45fea8dd552340f7bda64ec45e
Edson Porto da Silva <edsonporto88@gmail.com>
2021-08-16 15:55:03 -0300

    Update utils.py

--- Jupyter notebooks/utils.py
+++ Jupyter notebooks/utils.py
@@ -3,7 +3,16 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 def genGIF(x, y, figName, xlabel=[], ylabel=[], fram=200, inter=20):    
+    '''
+    :param x: x-axis values [np array]
+    :param y: y-axis values [np array]
+    :param figName: figure file name w/ folder path [string]
+    :param xlabel: xlabel [string]
+    :param ylabel: ylabel [string]
+    :param fram: number of frames [int]
+    :param inter: time interval between frames [milliseconds]
     
+    '''
     figAnin = plt.figure()
     ax      = plt.axes(xlim=(np.min(x), np.max(x)),\
                        ylim=(np.min(y)-0.1*np.max(np.abs(y)), np.max(y)+0.1*np.max(np.abs(y))))
