33491982fd4ecfeea1719fe34279ea9123309840
Edson Porto da Silva <edsonporto88@gmail.com>
2021-11-22 09:44:16 -0300

    update tutorial

--- Jupyter notebooks/Tutorial Sympy - Python para Circuitos Eletricos I .py
+++ Jupyter notebooks/Tutorial Sympy - Python para Circuitos Eletricos I .py
@@ -16,11 +16,11 @@
 
 # # *Tutorial Sympy - Python para Circuitos Elétricos I*
 #
-# Neste notebook, apresenta-se um guia de como utilizar o pacote **SymPy** do Python para implementar a maior parte das ferramentas matemáticas básicas utilizadas na disciplina de Circuitos Elétricos I.
+# Este notebook contém um guia resumo de como utilizar o pacote **SymPy** do Python para implementar a maior parte das ferramentas matemáticas básicas utilizadas na disciplina de Circuitos Elétricos I.
 
 # + [markdown] toc=true
 # <h1>Sumário<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"><li><span><a href="#Definindo-expressões-matemáticas" data-toc-modified-id="Definindo-expressões-matemáticas-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Definindo expressões matemáticas</a></span></li><li><span><a href="#Calculando-valores-numéricos-de-funções" data-toc-modified-id="Calculando-valores-numéricos-de-funções-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Calculando valores numéricos de funções</a></span></li><li><span><a href="#Arrendondamento-de-valores-numéricos" data-toc-modified-id="Arrendondamento-de-valores-numéricos-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Arrendondamento de valores numéricos</a></span></li><li><span><a href="#Derivadas-utilizando-sp.diff" data-toc-modified-id="Derivadas-utilizando-sp.diff-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Derivadas utilizando <em>sp.diff</em></a></span></li><li><span><a href="#Integrais-definidas-utilizando-sp.integrate" data-toc-modified-id="Integrais-definidas-utilizando-sp.integrate-5"><span class="toc-item-num">5&nbsp;&nbsp;</span>Integrais definidas utilizando <em>sp.integrate</em></a></span></li><li><span><a href="#Plotando-gráficos-com-symplot" data-toc-modified-id="Plotando-gráficos-com-symplot-6"><span class="toc-item-num">6&nbsp;&nbsp;</span>Plotando gráficos com symplot</a></span></li><li><span><a href="#Encontrando-numericamente-um-ponto-de-extremo-(máximo-ou-mínimo)-de-uma-função-com-sp.nsolve" data-toc-modified-id="Encontrando-numericamente-um-ponto-de-extremo-(máximo-ou-mínimo)-de-uma-função-com-sp.nsolve-7"><span class="toc-item-num">7&nbsp;&nbsp;</span>Encontrando numericamente um ponto de extremo (máximo ou mínimo) de uma função com <em>sp.nsolve</em></a></span></li><li><span><a href="#Resolvendo-um-sistema-de-equações-lineares" data-toc-modified-id="Resolvendo-um-sistema-de-equações-lineares-8"><span class="toc-item-num">8&nbsp;&nbsp;</span>Resolvendo um sistema de equações lineares</a></span></li></ul></div>
+# <div class="toc"><ul class="toc-item"><li><span><a href="#Definindo-expressões-matemáticas" data-toc-modified-id="Definindo-expressões-matemáticas-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Definindo expressões matemáticas</a></span></li><li><span><a href="#Calculando-valores-numéricos-de-funções" data-toc-modified-id="Calculando-valores-numéricos-de-funções-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Calculando valores numéricos de funções</a></span></li><li><span><a href="#Arrendondamento-de-valores-numéricos" data-toc-modified-id="Arrendondamento-de-valores-numéricos-3"><span class="toc-item-num">3&nbsp;&nbsp;</span>Arrendondamento de valores numéricos</a></span></li><li><span><a href="#Derivadas-utilizando-sp.diff" data-toc-modified-id="Derivadas-utilizando-sp.diff-4"><span class="toc-item-num">4&nbsp;&nbsp;</span>Derivadas utilizando <em>sp.diff</em></a></span></li><li><span><a href="#Integrais-definidas-utilizando-sp.integrate" data-toc-modified-id="Integrais-definidas-utilizando-sp.integrate-5"><span class="toc-item-num">5&nbsp;&nbsp;</span>Integrais definidas utilizando <em>sp.integrate</em></a></span></li><li><span><a href="#Plotando-gráficos-com-symplot" data-toc-modified-id="Plotando-gráficos-com-symplot-6"><span class="toc-item-num">6&nbsp;&nbsp;</span>Plotando gráficos com symplot</a></span></li><li><span><a href="#Encontrando-numericamente-um-ponto-de-extremo-(máximo-ou-mínimo)-de-uma-função-com-sp.nsolve" data-toc-modified-id="Encontrando-numericamente-um-ponto-de-extremo-(máximo-ou-mínimo)-de-uma-função-com-sp.nsolve-7"><span class="toc-item-num">7&nbsp;&nbsp;</span>Encontrando numericamente um ponto de extremo (máximo ou mínimo) de uma função com <em>sp.nsolve</em></a></span></li><li><span><a href="#Resolvendo-um-sistema-de-equações-lineares-com-sp.solve" data-toc-modified-id="Resolvendo-um-sistema-de-equações-lineares-com-sp.solve-8"><span class="toc-item-num">8&nbsp;&nbsp;</span>Resolvendo um sistema de equações lineares com <em>sp.solve</em></a></span></li></ul></div>
 # -
 
 import sympy as sp
@@ -38,22 +38,26 @@ i = sp.exp(-t)
 v = t*sp.exp(-t)*sp.cos(t)
 p = v*i
 
-symdisp('i(t) = ', i)
-symdisp('v(t) = ', v)
-symdisp('p(t) = ', p)
+symdisp('i(t) = ', i, 'A')
+symdisp('v(t) = ', v, 'V')
+symdisp('p(t) = ', p, 'W')
 # -
+
+# ```symdisp``` é uma função customizada (disponível no arquivo ```utils.py``` presente no repositório) para mostrar expressões definidas simbolicamente com SymPy. Para utilizar esta função num notebooks qualquer, faz-se necessário que o arquivo ```utils.py``` esteja no mesmo diretório em que o notebook está salvo.
+
+help(symdisp)
 
 # ### Calculando valores numéricos de funções
 
-symdisp('i(2) = ',  i.evalf(subs={t:2}) )
-symdisp('v(2) = ',  v.evalf(subs={t:2}) )
-symdisp('p(2) = ',  p.evalf(subs={t:2}) )
+symdisp('i(2) = ',  i.evalf(subs={t:2}), 'A')
+symdisp('v(2) = ',  v.evalf(subs={t:2}), 'V')
+symdisp('p(2) = ',  p.evalf(subs={t:2}), 'W')
 
 # ###  Arrendondamento de valores numéricos
 
-symdisp('i(2) = ',  round(i.evalf(subs={t:2}),3) )
-symdisp('v(2) = ',  round(v.evalf(subs={t:2}),3) )
-symdisp('p(2) = ',  round(p.evalf(subs={t:2}),3) )
+symdisp('i(2) = ',  round(i.evalf(subs={t:2}),3), 'A')
+symdisp('v(2) = ',  round(v.evalf(subs={t:2}),3), 'V')
+symdisp('p(2) = ',  round(p.evalf(subs={t:2}),3), 'W')
 
 # ### Derivadas utilizando *sp.diff*
 
@@ -102,6 +106,12 @@ symdisp('\int_{0}^{1} v(u)du =', sp.N(int_v, 2))
 symdisp('\int_{0}^{1} p(u)du =', sp.N(int_p, 2))
 
 # ### Plotando gráficos com symplot
+#
+# ```symplot``` é uma função customizada (disponível no arquivo ```utils.py``` presente no repositório) para plotar gráficos de funções definidas simbolicamente com SymPy. Para utilizar esta função num notebooks qualquer, faz-se necessário que o arquivo ```utils.py``` esteja no mesmo diretório em que o notebook está salvo.
+
+help(symplot)
+
+# **Exemplos**:
 
 # +
 intervalo = np.arange(0, 5, 0.01)
@@ -136,7 +146,7 @@ symdisp('t_{ext} =', round(text,3))
 symdisp('p_{ext} =', round(pext,3))
 # -
 
-# ### Resolvendo um sistema de equações lineares
+# ### Resolvendo um sistema de equações lineares com *sp.solve*
 
 # +
 # define as N variáveis desconhecidas
