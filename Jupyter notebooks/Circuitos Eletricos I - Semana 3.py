3cdc14a538c38639b6415254d27aa1dada7a547f
Edson Porto da Silva <edsonporto88@gmail.com>
2021-12-09 17:05:41 -0300

    update notebook semana 3

--- Jupyter notebooks/Circuitos Eletricos I - Semana 3.py
+++ Jupyter notebooks/Circuitos Eletricos I - Semana 3.py
@@ -7,17 +7,12 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.3
+#       jupytext_version: 1.13.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
-
-# + [markdown] toc=true
-# <h1>Table of Contents<span class="tocSkip"></span></h1>
-# <div class="toc"><ul class="toc-item"><li><span><a href="#Resolução-de-circuitos-utilizando-análise-nodal:-método-das-tensões-de-nó." data-toc-modified-id="Resolução-de-circuitos-utilizando-análise-nodal:-método-das-tensões-de-nó.-1"><span class="toc-item-num">1&nbsp;&nbsp;</span>Resolução de circuitos utilizando análise nodal: método das tensões de nó.</a></span><ul class="toc-item"><li><span><a href="#Procedimento-de-solução:" data-toc-modified-id="Procedimento-de-solução:-1.1"><span class="toc-item-num">1.1&nbsp;&nbsp;</span>Procedimento de solução:</a></span></li><li><span><a href="#Problema-1" data-toc-modified-id="Problema-1-1.2"><span class="toc-item-num">1.2&nbsp;&nbsp;</span>Problema 1</a></span></li><li><span><a href="#Solução-das-equações" data-toc-modified-id="Solução-das-equações-1.3"><span class="toc-item-num">1.3&nbsp;&nbsp;</span>Solução das equações</a></span></li><li><span><a href="#Cálculo-das-correntes" data-toc-modified-id="Cálculo-das-correntes-1.4"><span class="toc-item-num">1.4&nbsp;&nbsp;</span>Cálculo das correntes</a></span></li><li><span><a href="#Cálculo-das-potências" data-toc-modified-id="Cálculo-das-potências-1.5"><span class="toc-item-num">1.5&nbsp;&nbsp;</span>Cálculo das potências</a></span></li></ul></li><li><span><a href="#Resolução-de-circuitos-utilizando-análise-de-malhas:-correntes-de-malha." data-toc-modified-id="Resolução-de-circuitos-utilizando-análise-de-malhas:-correntes-de-malha.-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Resolução de circuitos utilizando análise de malhas: correntes de malha.</a></span><ul class="toc-item"><li><span><a href="#Procedimento-de-solução:" data-toc-modified-id="Procedimento-de-solução:-2.1"><span class="toc-item-num">2.1&nbsp;&nbsp;</span>Procedimento de solução:</a></span></li><li><span><a href="#Problema-2" data-toc-modified-id="Problema-2-2.2"><span class="toc-item-num">2.2&nbsp;&nbsp;</span>Problema 2</a></span></li><li><span><a href="#Solução-das-equações" data-toc-modified-id="Solução-das-equações-2.3"><span class="toc-item-num">2.3&nbsp;&nbsp;</span>Solução das equações</a></span></li><li><span><a href="#Cálculo-das-correntes" data-toc-modified-id="Cálculo-das-correntes-2.4"><span class="toc-item-num">2.4&nbsp;&nbsp;</span>Cálculo das correntes</a></span></li><li><span><a href="#Cálculo-das-potências" data-toc-modified-id="Cálculo-das-potências-2.5"><span class="toc-item-num">2.5&nbsp;&nbsp;</span>Cálculo das potências</a></span></li></ul></li></ul></div>
-# -
 
 from IPython.core.display import HTML
 from IPython.display import Image
@@ -52,11 +47,12 @@ HTML("""
 # ### Problema 1
 #
 # Aplicando o método das tensões de nó, determine todas as correntes e potências desenvolvidas por cada elemento no circuito a seguir:
-
-Image("./figures/J4C1.png", width=500)
+#
+# <img src="./figures/J4C1.png" width="500">
 
 # ### Solução das equações
 
+from utils import symdisp
 import sympy as sp
 import numpy as np
 
@@ -66,19 +62,24 @@ va, vb, vc, vd = sp.symbols('va, vb, vc, vd')
 
 # define os sistema de N equações
 eq1 = sp.Eq()             
-eq2 = sp.Eq()  
+eq2 = sp.Eq()
 eq3 = sp.Eq()
 eq4 = sp.Eq()
 
 # resolve o sistema
 soluc = sp.solve((eq1, eq2, eq3, eq4), dict=True)
+soluc = soluc[0]
 
-va = np.array([sol[va] for sol in soluc])
-vb = np.array([sol[vb] for sol in soluc]) 
-vc = np.array([sol[vc] for sol in soluc]) 
-vd = np.array([sol[vd] for sol in soluc]) 
+va = soluc[va]
+vb = soluc[vb]
+vc = soluc[vc]
+vd = soluc[vd]
 
-print('Solução do sistema:\n\n va = %.2f V,\n vb = %.2f V,\n vc = %.2f V,\n vd = %.2f V.' %(va, vb, vc, vd))
+print('Solução do sistema:')
+symdisp('v_a = ', round(va, 2), ' V')
+symdisp('v_b = ', round(vb, 2), ' V')
+symdisp('v_c = ', round(vc, 2), ' V')
+symdisp('v_d = ', round(vd, 2), ' V')
 # -
 
 # ### Cálculo das correntes
@@ -92,8 +93,14 @@ i6 =
 i1 = 
 i5 = 
 
-print('Correntes:\n\n i1 = %.2f A,\n i2 = %.2f A,\n i3 = %.2f A,\n i4 = %.2f A,\n i5 = %.2f A,\n i6 = %.2f A,\n ix = %.2f A.'\
-      %(i1, i2, i3, i4, i5, i6, ix))
+print('Correntes:')
+symdisp('i_1 = ', round(i1, 2), ' A')
+symdisp('i_2 = ', round(i2, 2), ' A')
+symdisp('i_3 = ', round(i3, 2), ' A')
+symdisp('i_4 = ', round(i4, 2), ' A')
+symdisp('i_5 = ', round(i5, 2), ' A')
+symdisp('i_6 = ', round(i6, 2), ' A')
+symdisp('i_x = ', round(ix, 2), ' A')
 # -
 
 # ### Cálculo das potências
@@ -125,30 +132,35 @@ print('Somatório das potências : %.2f W\n' %(pR+p10V+p05A+p12V+p2ix))
 # ### Problema 2
 #
 # Aplicando o método das correntes de malha, determine todas as correntes e potências desenvolvidas por cada elemento no circuito a seguir:
-
-Image("./figures/J4C2.png", width=500)
+#
+# <img src="./figures/J4C2.png" width="500">
 
 # ### Solução das equações
 
 # +
 # define as N variáveis desconhecidas
-ia, ib, ic, id = sp.symbols('ia, ib, ic, id')
+Ia, Ib, Ic, Id = sp.symbols('ia, ib, ic, id')
 
 # define os sistema de N equações
 eq1 = sp.Eq()             
-eq2 = sp.Eq()  
-eq3 = sp.Eq()
-eq4 = sp.Eq()
+eq2 = sp.Eq()             
+eq3 = sp.Eq()             
+eq4 = sp.Eq()  
 
 # resolve o sistema
 soluc = sp.solve((eq1, eq2, eq3, eq4), dict=True)
+soluc = soluc[0]
 
-ia = np.array([sol[ia] for sol in soluc])
-ib = np.array([sol[ib] for sol in soluc]) 
-ic = np.array([sol[ic] for sol in soluc]) 
-id = np.array([sol[id] for sol in soluc]) 
+Ia = soluc[Ia]
+Ib = soluc[Ib]
+Ic = soluc[Ic]
+Id = soluc[Id]
 
-print('Solução do sistema:\n\n ia = %.2f A,\n ib = %.2f A,\n ic = %.2f A,\n id = %.2f A.' %(ia, ib, ic, id))
+print('Correntes:')
+symdisp('i_a = ', round(Ia, 2), ' A')
+symdisp('i_b = ', round(Ib, 2), ' A')
+symdisp('i_c = ', round(Ic, 2), ' A')
+symdisp('i_d = ', round(Id, 2), ' A')
 # -
 
 # ### Cálculo das correntes
@@ -162,8 +174,14 @@ i6 =
 i1 = 
 i5 = 
 
-print('Correntes:\n\n i1 = %.2f A,\n i2 = %.2f A,\n i3 = %.2f A,\n i4 = %.2f A,\n i5 = %.2f A,\n i6 = %.2f A,\n ix = %.2f A.'\
-      %(i1, i2, i3, i4, i5, i6, ix))
+print('Correntes:')
+symdisp('i_1 = ', round(i1, 2), ' A')
+symdisp('i_2 = ', round(i2, 2), ' A')
+symdisp('i_3 = ', round(i3, 2), ' A')
+symdisp('i_4 = ', round(i4, 2), ' A')
+symdisp('i_5 = ', round(i5, 2), ' A')
+symdisp('i_6 = ', round(i6, 2), ' A')
+symdisp('i_x = ', round(ix, 2), ' A')
 # -
 
 # ### Cálculo das potências
