497e2ba56a399be3da91910f5fa483cb471361ba
Edson Porto da Silva <edsonporto88@gmail.com>
2024-08-29 22:45:44 -0300

    Update utils.py

--- circuit/utils.py
+++ circuit/utils.py
@@ -3,9 +3,51 @@ import numpy as np
 import sympy as sp
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib import rc
 from IPython.display import Math, display
 from sympy import lambdify
-from sympy.polys.partfrac import apart
+
+def set_preferences():
+    """
+    Set the preferences for matplotlib plots.
+    This function sets various preferences for matplotlib plots, such as font family, figure size, label size, grid lines, and legend style.
+    Returns:
+        None
+    """
+    # code implementation here
+
+    rc('font',**{'family':'serif','serif':['Times']})
+    rc('text', usetex=True)
+
+    plt.rcParams['figure.dpi'] = 200
+    plt.rcParams['figure.figsize'] = (5,2.5)
+
+    plt.rcParams['axes.labelsize'] = 12
+    plt.rcParams['xtick.labelsize'] = 9
+    plt.rcParams['ytick.labelsize'] = 9
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['font.size'] = 10
+
+    plt.rcParams['axes.linewidth'] =  0.5
+    plt.rcParams['grid.linewidth'] =  0.5
+    plt.rcParams['lines.linewidth'] =  0.5
+    plt.rcParams['lines.markersize'] =  2
+                    
+    # Grid lines
+    plt.rcParams['axes.grid'] =   False
+    plt.rcParams['axes.axisbelow'] =  False
+    plt.rcParams['grid.linestyle'] =  'dashed'
+    plt.rcParams['grid.color'] =   'k'
+    plt.rcParams['grid.alpha'] =   0.25
+    plt.rcParams['grid.linewidth'] =   0.5
+
+    # Legend
+    plt.rcParams['legend.frameon'] =   False
+    plt.rcParams['legend.framealpha'] =   0.25
+    plt.rcParams['legend.fancybox'] =   False
+    plt.rcParams['legend.numpoints'] =   1
+
+    return None
 
 def symdisp(expr, var=None, unit=None):
     """
